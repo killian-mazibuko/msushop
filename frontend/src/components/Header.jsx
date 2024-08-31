1f18ff75308b6a3fdd56d2015c50a00ac7541e69
Killian Mazibuko <mazibukok@staff.msu.ac.za>
2024-08-31 23:58:49 +0200

    Modified logo size

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -35,8 +35,8 @@ const Header = () => {
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <Navbar.Brand as={Link} to='/'>
-            <img src='images/logo.png' />
-            MSU Store
+            <img src='images/logo.png' style={{height:50, width: 50}}/>
+            {'  '}MSU Store
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
